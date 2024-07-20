81f4d7fb15efc23c13990984ace7c30c71e2d608
Alexander Treml <alexander@treml.de>
2024-07-20 17:37:16 +0200

    improved UI

--- app/src/main/java/com/alexander_treml/asttuner/MainActivity.kt
+++ app/src/main/java/com/alexander_treml/asttuner/MainActivity.kt
@@ -9,6 +9,7 @@ import androidx.activity.compose.setContent
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.border
 import androidx.compose.foundation.gestures.Orientation
 import androidx.compose.foundation.gestures.draggable
 import androidx.compose.foundation.gestures.rememberDraggableState
@@ -20,6 +21,7 @@ import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.automirrored.filled.ArrowForward
@@ -27,6 +29,8 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.IconButtonDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -38,6 +42,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
@@ -45,16 +50,17 @@ import androidx.compose.ui.graphics.Path
 import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.platform.LocalView
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-import com.alexander_treml.asttuner.ui.theme.ASTTunerTheme
+import com.alexander_treml.asttuner.ui.theme.AppTheme
 import kotlin.math.min
 
 // TODO fix swipe bug that causes button issues
-// TODO UI previews
 // TODO cleanly handle the transition from double precision in the backend to single precision in the UI
-// TODO use the theme (color scheme: https://material-foundation.github.io/material-theme-builder/)
+// TODO better color theme (also check color scheme for accessibility)
+// TODO reuse modifiers
 class MainActivity : ComponentActivity() {
     private val listener = Listener(this)
     private var tuning = Tuning()
@@ -66,15 +72,30 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
 
         setContent {
-            ASTTunerTheme {
+            AppTheme {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
                 ) {
                     Column {
                         TuningPanel(
-                            modifier = Modifier.weight(1f)
+                            modifier = Modifier
+                                .weight(1f)
+                                .border(
+                                    width = 2.dp,
+                                    color = MaterialTheme.colorScheme.onBackground,
+                                    shape = RoundedCornerShape(8.dp)
+                                )
+                                .clip(RoundedCornerShape(8.dp))
                         )
-                        FrequencyDisplay()
+                        FrequencyDisplay(modifier = Modifier
+                            .border(
+                                width = 2.dp,
+                                color = MaterialTheme.colorScheme.onBackground,
+                                shape = RoundedCornerShape(8.dp)
+                            )
+                            .clip(RoundedCornerShape(8.dp))
+                        )
                     }
                 }
             }
@@ -104,12 +125,12 @@ class MainActivity : ComponentActivity() {
     // Display the listener results
     // Has a debug mode to see the internal processing
     @Composable
-    fun FrequencyDisplay() {
+    fun FrequencyDisplay(modifier: Modifier) {
         val frequency by listener.frequency.collectAsState()
         var debugMode by remember { mutableStateOf(false) }
 
         Box(
-            modifier = Modifier
+            modifier = modifier
                 .aspectRatio(2f)
                 .draggable(
                     orientation = Orientation.Horizontal,
@@ -125,7 +146,7 @@ class MainActivity : ComponentActivity() {
             if (debugMode) {
                 DebugView(frequency)
             } else {
-                NeedleDisplay(frequency)
+                NeedleDisplay(frequency, arcColor = MaterialTheme.colorScheme.primary)
             }
         }
 
@@ -147,7 +168,11 @@ class MainActivity : ComponentActivity() {
             horizontalArrangement = Arrangement.Center
         ) {
             IconButton(
-                onClick = { note.shift(-1) },
+                onClick = { note.shift(-1); selectedNote = note },
+                colors = IconButtonDefaults.iconButtonColors(
+                    containerColor = MaterialTheme.colorScheme.background,
+                    contentColor = MaterialTheme.colorScheme.onBackground
+                ),
                 modifier = Modifier.weight(0.25f)
             ) {
                 Icon(Icons.AutoMirrored.Filled.ArrowBack, contentDescription = "Decrease Note")
@@ -157,14 +182,27 @@ class MainActivity : ComponentActivity() {
                 onClick = { selectedNote = note },
                 modifier = Modifier.weight(1f),
                 colors = ButtonDefaults.buttonColors(
-                    containerColor = if (selectedNote == note ) Color.Green else Color.Cyan,
+                    containerColor =
+                        if (selectedNote == note )
+                            MaterialTheme.colorScheme.primary
+                        else
+                            MaterialTheme.colorScheme.secondary,
+                    contentColor =
+                        if (selectedNote == note )
+                            MaterialTheme.colorScheme.onPrimary
+                        else
+                            MaterialTheme.colorScheme.onSecondary
                 ),
             ) {
                 Text(note.name)
             }
 
             IconButton(
-                onClick = { note.shift(1) },
+                onClick = { note.shift(1); selectedNote = note },
+                colors = IconButtonDefaults.iconButtonColors(
+                    containerColor = MaterialTheme.colorScheme.background,
+                    contentColor = MaterialTheme.colorScheme.onBackground
+                ),
                 modifier = Modifier.weight(0.25f)
             ) {
                 Icon(Icons.AutoMirrored.Filled.ArrowForward, contentDescription = "Increase Note")
@@ -173,7 +211,7 @@ class MainActivity : ComponentActivity() {
     }
 
     @Composable
-    fun NeedleDisplay(frequency: Double) {
+    fun NeedleDisplay(frequency: Double, arcColor: Color) {
         val angle by animateFloatAsState(
             selectedNote
                 .getDistance(frequency)
@@ -183,11 +221,9 @@ class MainActivity : ComponentActivity() {
         )
 
         Canvas(
-            modifier = Modifier
-                .aspectRatio(2f)
-                .fillMaxSize()
+            modifier = Modifier.aspectRatio(2f)
         ) {
-            val strokeWidth = 20f
+            val strokeWidth = 10f
             val offset = strokeWidth / 2
             val radius = min(size.height, size.width / 2f)
 
@@ -202,22 +238,42 @@ class MainActivity : ComponentActivity() {
                 strokeWidth = 4f
             )
 
+            // Draw ticks
+            var tickAngle = 0.0
+            val tickInset = 0.9f
+            // TODO use repeat instead of for with constant values
+            repeat(19) {
+                tickAngle += 0.1f * 90f
+                drawLine(
+                    color = arcColor,
+                    start = Offset(
+                        x = size.width / 2 + (radius * tickInset)  * kotlin.math.cos(Math.toRadians(180.0 - tickAngle)).toFloat(),
+                        y = size.height - (radius * tickInset) * kotlin.math.sin(Math.toRadians(180.0 - tickAngle)).toFloat()
+                    ),
+                    end = Offset(
+                        x = size.width / 2 + radius * kotlin.math.cos(Math.toRadians(180.0 - tickAngle)).toFloat(),
+                        y = size.height - radius * kotlin.math.sin(Math.toRadians(180.0 - tickAngle)).toFloat()
+                    ),
+                    strokeWidth = 8f
+                )
+            }
+
             // Draw the needle
             drawLine(
                 color = Color.Red,
                 start = Offset(size.width / 2, size.height),
                 end = Offset(
-                    x = size.width / 2 + radius * kotlin.math.cos(Math.toRadians((180.0 - angle.toDouble()))).toFloat(),
-                    y = size.height - radius * kotlin.math.sin(Math.toRadians((180.0 - angle.toDouble()))).toFloat()
+                    x = size.width / 2 + radius * kotlin.math.cos(Math.toRadians(180.0 - angle)).toFloat(),
+                    y = size.height - radius * kotlin.math.sin(Math.toRadians(180.0 - angle)).toFloat()
                 ),
                 strokeWidth = 8f
             )
 
             // Draw the dial
             drawArc(
-                color = Color.Gray,
-                startAngle = 180f,
-                sweepAngle = 180f,
+                color = arcColor,
+                startAngle = 175f,
+                sweepAngle = 190f,
                 useCenter = false,
                 topLeft = Offset(offset, offset),
                 size = Size(size.width - strokeWidth, (size.height - strokeWidth) * 2),
@@ -266,7 +322,7 @@ class MainActivity : ComponentActivity() {
                             }
                         }
                     },
-                    color = Color.Blue,
+                    color = Color.Red,
                     style = Stroke(width = 2.dp.toPx())
                 )
 
@@ -275,7 +331,7 @@ class MainActivity : ComponentActivity() {
                     val x = marker * spacing
 
                     drawLine(
-                        color = Color.Red,
+                        color = Color.Green,
                         start = Offset(x, 0f),
                         end = Offset(x, size.height),
                         strokeWidth = 1f
@@ -328,4 +384,37 @@ class MainActivity : ComponentActivity() {
             }
             .show()
     }
+
+    @Preview(showBackground = true)
+    @Composable
+    fun AppPreview() {
+        AppTheme {
+            Surface(
+                modifier = Modifier.fillMaxSize(),
+                color = MaterialTheme.colorScheme.background
+            ) {
+                Column {
+                    TuningPanel(
+                        modifier = Modifier
+                            .weight(1f)
+                            .border(
+                                width = 2.dp,
+                                color = MaterialTheme.colorScheme.onBackground,
+                                shape = RoundedCornerShape(8.dp)
+                            )
+                            .clip(RoundedCornerShape(8.dp))
+                    )
+                    FrequencyDisplay(modifier = Modifier
+                        .border(
+                            width = 2.dp,
+                            color = MaterialTheme.colorScheme.onBackground,
+                            shape = RoundedCornerShape(8.dp)
+                        )
+                        .clip(RoundedCornerShape(8.dp))
+                    )
+                }
+            }
+        }
+    }
 }
+
